b343f9785ed364e0f31eed7e2ee6fd39122d7003
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-07-07 12:41:34 +0300

    step6 основы

--- modulesapp/app.js
+++ modulesapp/app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(request, response) {
-    response.end("Hello from express")
+    console.log("Hello");
+    response.end("Hello from express12")
 });
 
 app.listen(3000);

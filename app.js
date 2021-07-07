2370ded63d55674f62d60c407ae649603e151166
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-07-07 10:45:19 +0300

    основы

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-const http = require("http");
-http.createServer(function(request,response){
-     
-    response.end("Hello NodeJS!");
-     
-}).listen(3000, "127.0.0.1",function(){
-    console.log("Сервер начал прослушивание запросов на порту 3000");
-});
+const os = require("os");
+const gretting = require("./gretting");
+
+let userName = os.userInfo().username;
+
+console.log(`Дата запроса: ${gretting.date}`);
+console.log(gretting.getMessage(userName));

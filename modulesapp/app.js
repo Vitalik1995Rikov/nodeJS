f55b334843972fc61bf8eabbf11105be76f8af99
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-07-08 14:25:26 +0300

    задача с файлом

--- modulesapp/app.js
+++ modulesapp/app.js
@@ -2,7 +2,17 @@ const fs = require("fs");
 
 let fileContent = fs.readFileSync("hello.txt", "utf8");
 let arr = Array.from(fileContent);
-arr = arr.map((value) => value*2 );
-fs.writeFileSync("hello.txt", "arr");
-let data = fs.readFileSync("hello.txt", "utf8");
-console.log(data);
+let newArr = [];
+for (value of arr) {
+    let valueNew = Number(value);
+    newArr.push(valueNew);
+}
+
+let newArrLast = [];
+for (let i = 0; i < newArr.length; i++) {
+    if (newArr[i] !== 0) {
+        newArrLast.push(newArr[i])
+    }
+}
+
+console.log(newArrLast.length);
